b65fe07c32e5f470201df324f4d63c954e4c4ecd
Jean Chen <jeanchen.nz@gmail.com>
2024-07-08 16:48:05 +1200

    Browser, Login, UI control and  Child window handling
    Browser, Login, UI control and  Child window handling

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -43,11 +43,24 @@ test.only('Child window handling', async({browser})=>
 {
     const context = await browser.newContext();
     const page = await context.newPage();
+    const userName = page.locator('#username');
     await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
     const documentLink = page.locator("[href*='documents-request']");
-    const page2 = context.waitForEvent('page');
-    documentLink.click();
+
+    const [newPage] = await Promise.all([
+    context.waitForEvent('page'),//listeen for any new page
+    documentLink.click(),
+    ]);//new page is oppened
     
+    const text = await newPage.locator(".red").textContent();
+    const arrayText = text.split("@");
+    const domain = arrayText[1].split(" ")[0];
+    console.log(domain);
+    await page.locator("#username").fill(domain);
+    await page.pause();
+    console.log(await page.locator("#username").textContent());
+
+
 
     
 
