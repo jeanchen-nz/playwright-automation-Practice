72202f69d1b99c0fbdc3d4929b4c5d8be825d5b4
Jean Chen <jeanchen.nz@gmail.com>
2024-07-11 00:22:56 +1200

    find order from orderHistory list

--- tests/ClientApp.spec.js
+++ tests/ClientApp.spec.js
@@ -1,4 +1,5 @@
 const {test, expect} = require('@playwright/test');
+const exp = require('constants');
 
 
 test('Browser Context-Validating Error login', async({page})=>
@@ -110,6 +111,21 @@ test.only('Client App Login', async({page})=>
             await expect(page.locator(".hero-primary")).toHaveText(" Thankyou for the order. ")
             const orderId = await page.locator(".em-spacer-1 .ng-star-inserted").textContent();
             console.log(orderId);
+            await page.locator("button[routerlink*='myorders']").click();
+            await page.locator("tbody").waitFor();
+            const rows = await page.locator("tbody tr");
+
+            for(let i = 0; i < await rows.count(); ++i)
+            {
+                const rowOrderId = await rows.nth(i).locator("th").textContent();
+                if (orderId.includes(rowOrderId))
+                {
+                    await rows.nth(i).locator("button").first().click();
+                    break;
+                }
+            }
+            const orderIdDetails =await page.locator(".col-text").textContent();
+            expect(orderId.includes(orderIdDetails)).toBeTruthy();
 
 
     });
